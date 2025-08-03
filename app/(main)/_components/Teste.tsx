4f2c672a85d3832ec143a466332495950d6a81da
Kevin Demetro <albinodemetrok@gmail.com>
2025-08-03 20:06:19 -0300

    define posição da sidedrawer

--- app/(main)/_components/Teste.tsx
+++ app/(main)/_components/Teste.tsx
@@ -3,16 +3,14 @@ import SideDrawer from "@/app/_components/SideDrawer";
 
 function Teste() {
   return (
-    <div>
-      <SideDrawer>
-        <SideDrawer.Open name="teste">
-          <button>teste</button>
-        </SideDrawer.Open>
-        <SideDrawer.Window name="teste">
-          <div>aaaaaaaaaaa</div>
-        </SideDrawer.Window>
-      </SideDrawer>
-    </div>
+    <SideDrawer>
+      <SideDrawer.Open name="teste">
+        <button>teste</button>
+      </SideDrawer.Open>
+      <SideDrawer.Window name="teste" openPosition="right">
+        <div>aaaaaaaaaaa</div>
+      </SideDrawer.Window>
+    </SideDrawer>
   );
 }
 
--- app/_components/SideDrawer.tsx
+++ app/_components/SideDrawer.tsx
@@ -33,6 +33,7 @@ interface OpenProps {
 interface WindowProps {
   children: ReactElement<{ onCloseModal?: () => void }>;
   name: string;
+  openPosition: string;
 }
 
 interface SideDrawerComponent extends FC<SideDrawerProps> {
@@ -56,13 +57,38 @@ const Open: FC<OpenProps> = ({ children, name }) => {
   return cloneElement(children, { onClick: () => context?.open(name) });
 };
 
-const Window: FC<WindowProps> = ({ children, name }) => {
+const Window: FC<WindowProps> = ({ children, name, openPosition = "left" }) => {
   const context = useContext(SideDrawerContext);
   if (name !== context?.openName) return null;
   return (
-    <div>
-      <button onClick={context?.close}>fechar</button>
-      {children}
+    <div className="fixed top-0 left-0 w-[100%] h-full bg-backdrop backdrop-blur-[4px] z-[1000]">
+      <div
+        className={`
+        ${
+          openPosition === "left"
+            ? "bg-amber-400 w-[70%] h-full fixed left-0 top-0"
+            : ""
+        }
+        ${
+          openPosition === "right"
+            ? "bg-amber-400 w-[70%] h-full fixed right-0 top-0"
+            : ""
+        }
+        ${
+          openPosition === "top"
+            ? "bg-amber-400 h-[50%] w-full fixed top-0 left-0"
+            : ""
+        }
+        ${
+          openPosition === "bottom"
+            ? "bg-amber-400 h-[50%] w-full fixed bottom-0 left-0"
+            : ""
+        }
+      `}
+      >
+        <button onClick={context?.close}>fechar</button>
+        {children}
+      </div>
     </div>
   );
 };
