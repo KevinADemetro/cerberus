ef0aba83df65280aaf874d26b3ed3985ce37b5a7
Kevin Demetro <albinodemetrok@gmail.com>
2025-08-12 18:41:23 -0300

    cria carrinho e salva id no cookie

---
+++ prisma/migrations/20250812203048_init/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE "public"."Cart" ALTER COLUMN "userId" DROP NOT NULL,
+ALTER COLUMN "updatedAt" DROP NOT NULL;
--- src/actions/cart.tsx
+++ src/actions/cart.tsx
@@ -1,4 +1,35 @@
 "use server";
 import { Prisma } from "@/generated/prisma";
+import { cookies } from "next/headers";
+import prisma from "../lib/prisma";
 
-export async function handleAddToCart(variant: Prisma.ProductVariantGetPayload<object>) {}
+export async function handleAddToCart(variant: Prisma.ProductVariantGetPayload<object>) {
+  const cookieStore = await cookies();
+  const cartUuid = await cookieStore.get("cartUuid");
+  let cart;
+  if (cartUuid) {
+    cart = await prisma.cart.findUniqueOrThrow({ where: { id: cartUuid.value } });
+    await prisma.cart.update({
+      where: { id: cartUuid.value },
+      data: {
+        updatedAt: new Date(),
+      },
+    });
+  } else {
+    cart = await prisma.cart.create({
+      data: {
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    });
+    cookieStore.set("cartUuid", cart.id);
+  }
+
+  await prisma.cartItem.create({
+    data: {
+      cartId: cart.id,
+      productVariantId: variant.id,
+      quantity: 1,
+    },
+  });
+}
