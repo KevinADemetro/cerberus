14ded27698e6ed9de12413cb9a9d2c79c010826c
Kevin Demetro <albinodemetrok@gmail.com>
2025-08-17 16:17:57 -0300

    Corrije bug ao procurar em uma página que não esteja dentro de /nav

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,8 +1,21 @@
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
-
+import { useRouter } from "next/navigation";
 function SearchBar() {
+  const router = useRouter();
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const term = formData.get("term") as string;
+
+    if (term) {
+      router.push(`/nav?term=${encodeURIComponent(term)}`);
+    }
+  }
   return (
-    <form className="w-full flex bg-gray-100 rounded-full p-1 focus-within:outline-2 focus-within:outline-gray-300 focus-within:bg-gray-300">
+    <form
+      onSubmit={handleSubmit}
+      className="w-full flex bg-gray-100 rounded-full p-1 focus-within:outline-2 focus-within:outline-gray-300 focus-within:bg-gray-300"
+    >
       <button className="rounded-full bg-background p-2">
         <MagnifyingGlassIcon className="size-4" />
       </button>
